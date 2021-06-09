fe6d720054f9f18c60d926479fbb53eed6178edf
ashishs <cushivas@gmail.com>
2021-06-08 23:54:51 -0400

    Fixed Routing

--- src/screens/Router.js
+++ src/screens/Router.js
@@ -1,17 +1,19 @@
 import { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import Home from "./home/home";
+import Home from "./home/Home";
 
 class Router extends Component {
+    baseUrl= '';
     constructor() {
         super();
+        this.baseUrl = '';
     }
 
     render(){
         return(
             <BrowserRouter>
                 <div className="main-container">
-                    <Route exact path="/" render={(props)=><Home {...props}/>}> </Route>
+                <Route exact path='/' render={(props) => <Home {...props} accessToken={this.baseUrl} />} />
                 </div>
             </BrowserRouter>
         )
